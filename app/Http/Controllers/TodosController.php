cc82d82d75095666574118df94e7e076352f3b96
Sergio Gil Pérez <GaskaBur@gmail.com>
2023-03-25 17:28:34 +0100

    start

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodosController extends Controller
+{
+    //Poner la tipo Request es obligatoria.
+    public function store(Request $request) {
+        $request->validate([
+            'title' => 'required|min:3'
+        ]);
+
+        $todo = new Todo;
+        $todo->title = $request->title;
+        $todo->save();
+
+        return redirect()->route('todos')->with('success', 'Tarea creada');
+
+    }
+}
